83f4ad89d0e0cd888357c7064fe599c6f07dec9e
Fenomen-Alex <alexfenomen@gmail.com>
2021-06-07 20:47:32 +0300

    Updates

---
+++ precache-manifest.874387a594a8153bed05f0aaaca6a3cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59803472ff1cf2e99f2a3dcf5904820c",
+    "url": "/Pomodoro_Timer_start/index.html"
+  },
+  {
+    "revision": "6c20c2161f3557e7838a",
+    "url": "/Pomodoro_Timer_start/static/css/main.d2ce90d3.chunk.css"
+  },
+  {
+    "revision": "a83b866c1c0f576d1986",
+    "url": "/Pomodoro_Timer_start/static/js/2.5b385fb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pomodoro_Timer_start/static/js/2.5b385fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c20c2161f3557e7838a",
+    "url": "/Pomodoro_Timer_start/static/js/main.6eddcf3b.chunk.js"
+  },
+  {
+    "revision": "60cfc2591556d51e4800",
+    "url": "/Pomodoro_Timer_start/static/js/runtime-main.4f5481c1.js"
+  }
+]);
